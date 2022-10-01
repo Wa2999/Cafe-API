22969d3c6909f44deb8f0c3ce79baa1214f04d78
wejdan alghamdi <92243050+Wa2999@users.noreply.github.com>
2022-10-01 18:49:41 +0300

    ok

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     default:
       "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
   },
-
+  // emailVerified: Boolean,
   reviews: [
     {
       type: mongoose.Types.ObjectId,
